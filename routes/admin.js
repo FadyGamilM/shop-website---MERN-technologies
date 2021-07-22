3d265c61b704282acd7724aac1190e89d5030ad1
fady gamil mahrous masoud <1700961@eng.asu.edu.eg>
2021-07-22 19:01:59 +0200

    routes after adding the controller to handle the code

--- routes/admin.js
+++ routes/admin.js
@@ -8,11 +8,17 @@ const products_controllers=require('../controllers/products');
 
 // these routes reached by the admin, as admin who is the only one who can add new product
 
-// so he is the one that can visit add-product route
+// route handler for GET request to /admin/add-product URL
 router.get('/add-product', products_controllers.get_add_products_page);
 
-// and when he create POST request from /add-product this route handle this operation
+// route handler for POST request to /admin/add-product URL
 router.post('/add-product',products_controllers.post_product);
+
+// route handler for GET request to /admin/edit-product URL
+router.get('/edit-product',products_controllers.get_edit_product_page);
+
+// route handler for GET request to /admin/products-list URL [ADMIN point of view]
+router.get('/products-list',products_controllers.get_products_list);
 
 module.exports=router;
 
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,29 @@ const products=adminRoutes_Data.products;
 
 // require controllers..
 const products_controllers=require('../controllers/products');
+const { route } = require('./admin');
 
-// when we write the controller we don't execute it, we just write its name.
-router.get('/home',products_controllers.get_all_products);
+// ==> when we write the controller we don't execute it, we just write its name.
+
+// route for page that shows all products to users [USER point of view to all products]
+router.get('/shop',products_controllers.get_all_products);
+
+// route for GET request for cart URL
+router.get('/cart',products_controllers.get_cart_page);
+
+//route for GET request for checkout URL
+router.get('/checkout',products_controllers.get_checkout_page);
+
+//route for GET request for home URL
+router.get('/home',products_controllers.get_home_page);
+
+//route for GET request for product details URL
+router.get('/product-details',products_controllers.get_product_details_page);
+
+// route for GET rqeuest for specific product details page (i.e /products/:1 )
+////// important note that this type of routes called dynamic routes, so if we have any specific routes
+////// such as /products/delete ==> we have to put our dynamic route after all those routes that start with /products/ANY_THING
+router.get('/products/:productID',products_controllers.get_specific_product_details_page);
+
 
 module.exports=router;
